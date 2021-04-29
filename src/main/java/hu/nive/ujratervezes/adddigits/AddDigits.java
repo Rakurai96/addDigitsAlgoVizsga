87aba4d5b0a636d551f669ddea99ac8fa2f83dde
Bajnóczi István <balbianello96@gmail.com>
2021-04-29 10:30:53 +0200

    Add digits are complere, all tests passed.

--- src/main/java/hu/nive/ujratervezes/adddigits/AddDigits.java
+++ src/main/java/hu/nive/ujratervezes/adddigits/AddDigits.java
@@ -1,5 +1,23 @@
 package hu.nive.ujratervezes.adddigits;
 
 public class AddDigits {
-    //todo: implement your solution here
+
+    public int addDigits(String input) {
+        if(input == null) {
+            return -1;
+        }
+        if(input.isEmpty()) {
+            return -1;
+        }
+
+        int sum = 0;
+        for(int i = 0; i < input.length(); i++) {
+            if(Character.isDigit(input.charAt(i))) {
+                int num = Integer.parseInt(input.substring(i, i + 1));
+                sum += num;
+            }
+        }
+
+        return sum;
+    }
 }
